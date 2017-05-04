ac2b75633d209975022356a85555a7c8f89afc2f
Alessandro Bonometti <bauno@bauniga.baita>
2017-05-04 19:26:44 +0200

    Hmmm

--- InventoryItem/Aggregate.fs
+++ InventoryItem/Aggregate.fs
@@ -20,20 +20,21 @@ type Aggregate<'TState, 'TCommand, 'TEvent> = {
 type Id = System.Guid
 
 /// Creates a persistent, async command handler for an aggregate given load and commit functions.
-let makeHandler (aggregate:Aggregate<'TState, 'TCommand, 'TEvent>) (load: System.Type * Id -> Async<obj seq>, commit: Id * int -> obj -> Async<Choice<unit, string list>>) =
+let makeHandler (aggregate:Aggregate<'TState, 'TCommand, 'TEvent>) (load: System.Type * Id -> Async<Choice<obj seq,string>>, commit: Id * int -> obj -> Async<Choice<unit, string list>>) =
     fun (id,version) command -> async {
         let! events = load (typeof<'TEvent>,id)
-        let events = events |> Seq.cast :> 'TEvent seq
-        let state = Seq.fold aggregate.apply aggregate.zero events
-        let event = aggregate.exec state command
-        match event with
-        | Choice1Of2 event ->
-            let! res  = event |> commit (id,version)
-            match res with
-            | Choice1Of2 x -> return Choice1Of2 x
-            | Choice2Of2 e -> return Choice2Of2 e
-        | Choice2Of2 errors ->
-            return errors |> Choice2Of2
+        let pippo = match events with
+                    | Choice2Of2 s -> Choice2Of2 s
+                    | Choice1Of2 es -> let events = es |> Seq.cast :> 'TEvent seq        
+                                       let state = Seq.fold aggregate.apply aggregate.zero events
+                                       let event = aggregate.exec state command
+                                       let event = aggregate.exec state command
+                                       match event with
+                                       | Choice1Of2 event -> let res  = event |> commit (id,version) |> Async.RunSynchronously
+                                                             match res with
+                                                             | Choice1Of2 x -> Choice1Of2 x                                                                               
+                                                             | Choice2Of2 e -> Choice2Of2 e.[0]                                                 
+        return pippo                
     }
 
 /// Creates a persistent command handler for an aggregate given load and commit functions.
